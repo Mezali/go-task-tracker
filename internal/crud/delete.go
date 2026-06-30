a8b4f6eba6f10d823f78ffdd74620e8e93536621
Daniel <danieldelimamazali@hotmail.com>
2026-06-30 16:30:38 -0300

    finished Delete

--- internal/crud/delete.go
+++ internal/crud/delete.go
@@ -1,5 +1,23 @@
 package crud
 
-func DeleteTask(TaskId uint) {
+import (
+	"encoding/json"
+
+	"github.com/Mezali/go-task-tracker/internal/models"
+)
+
+var model []models.Task
+
+func DeleteTask(File []byte, TaskId uint) []byte {
+	json.Unmarshal(File, &model)
+
+	for index, value := range model {
+		if value.Id == TaskId {
+			model = append(model[:index], model[index+1:]...)
+			break
+		}
+	}
+	model, _ := json.MarshalIndent(model, "", "  ")
 	
+	return model
 }
